072bc1031cdbfb26f9632c75e1adfdd6a45d0a15
sktan888 <402duchess@gmail.com>
2024-12-22 07:13:55 +0000

    logging

--- myLib/mylog.py
+++ myLib/mylog.py
@@ -13,7 +13,7 @@ def log_to_file(msg):
     """
 
     logging.basicConfig(
-        filename="mylog.log",
+        filename="info.log",
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
     )
